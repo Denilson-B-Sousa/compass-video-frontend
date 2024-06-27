2a3a6ca89c1b2723434e5ac12bea6a5a8d54ae17
Igor Baroni <igorbaroni77@gmail.com>
2024-06-27 14:03:32 -0300

    feat: home interface

--- compass-video-app/src/pages/Home/index.tsx
+++ compass-video-app/src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
-export function Home(){
+export function Home() {
   return (
-    <>
-      <div className='bg-neutral-600 p-8 h-screen'>home</div>
-    </>
-  )
+    <div className="bg-main-movie pt-60 md:pt-0">
+      <div className="h-[130vh]">
+        <div className=" h-full p-8 flex items-center">
+          <div className="md:w-1/2 flex flex-col gap-3">
+            <h1 className="text-applications-high-emphasis font-worksans text-5xl font-bold tracking-wide">
+              Five Nights at Freddy's
+            </h1>
+            <p className="text-applications-high-emphasis font-worksans">
+              2021 • 1 h 41 min
+            </p>
+            <p className="text-applications-high-emphasis font-worksans text-sm">
+              Drama, Sci-Fi & Fantasy
+            </p>
+            <p className="text-applications-high-emphasis font-worksans text-lg">
+              Recently fired and desperate for work, a troubled young man named
+              Mike agrees to take a position as a night security guard at an
+              abandoned theme restaurant: Freddy Fazbear's Pizzeria. But he soon
+              discovers that nothing at Freddy's is what it seems.
+            </p>
+            <div className="mt-5 flex flex-col md:flex-row gap-3">
+              <h2 className="text-white">Botão Ver Agora</h2>
+              <h2 className="text-white">Botão Mais Informações</h2>
+              <h2 className="text-white">Botão Adicionar a Lista</h2>
+              <h2 className="text-white">Botão Salvar aos Favoritos</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-8 pb-8 md:p-8">
+        <h2 className="text-white">Carrouseis</h2>
+        <h2 className="text-white">Carrouseis</h2>
+        <h2 className="text-white">Carrouseis</h2>
+        <h2 className="text-white">Carrouseis</h2>
+      </div>
+    </div>
+  );
 }
