62a52ca0cedc58a7636afab9ef7545f9d63f5efe
Igor Baroni <igorbaroni77@gmail.com>
2024-06-27 15:32:22 -0300

    feat: tooltip

--- compass-video-app/src/Components/PageButtons/index.tsx
+++ compass-video-app/src/Components/PageButtons/index.tsx
@@ -3,10 +3,17 @@ import playblack from "@assets/Images/icon/play-black.svg";
 import info from "@assets/Images/icon/info.svg";
 import star from "@assets/Images/icon/star.svg";
 import plus from "@assets/Images/icon/plus.svg";
+import { Tooltip } from "react-tooltip";
 
 export function PageButtons() {
   return (
     <div className="mt-5 flex flex-col md:flex-row gap-3">
+      <Tooltip
+        id="my-tooltip"
+        place="top"
+        className="font-worksans text-neutral-600 font-semibold"
+      />
+
       <Button secondary size="secoundary">
         <img src={playblack} alt="" className="mr-3" />
         Ver Agora
