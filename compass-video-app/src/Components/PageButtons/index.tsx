371f940f1abb606050c24d0fcbb4ff71b32c7f25
Igor Baroni <igorbaroni77@gmail.com>
2024-06-27 15:57:29 -0300

    feat: tooltip messages

--- compass-video-app/src/Components/PageButtons/index.tsx
+++ compass-video-app/src/Components/PageButtons/index.tsx
@@ -4,8 +4,26 @@ import info from "@assets/Images/icon/info.svg";
 import star from "@assets/Images/icon/star.svg";
 import plus from "@assets/Images/icon/plus.svg";
 import { Tooltip } from "react-tooltip";
+import { useState } from "react";
+import { Check } from "@phosphor-icons/react";
 
 export function PageButtons() {
+
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [isOnList, setIsOnList] = useState<boolean>(false);
+
+  const handleAddToFavorites = () => {
+    if(!isFavorite){
+      setIsFavorite(true);
+    } else setIsFavorite(false);
+  }
+
+  const handleAddToList = () => {
+    if(!isOnList){
+      setIsOnList(true);
+    } else setIsOnList(false);
+  }
+
   return (
     <div className="mt-5 flex flex-col md:flex-row gap-5">
       <Tooltip
@@ -23,24 +41,57 @@ export function PageButtons() {
         Mais Informações
       </Button>
       <div className="flex gap-4 items-center">
-        <Button
+
+         {!isOnList ? (
+          <Button
           outlinedSecondary
           size="default"
           data-tooltip-id="my-tooltip"
           data-tooltip-content="Adicionar à “assistir mais tarde”"
           data-tooltip-variant="light"
+          onClick={handleAddToList}
         >
           <img src={plus} />
         </Button>
-        <Button
+        ) : (
+          <Button
+          outlinedSecondary
+          size="default"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Em “assistir mais tarde”"
+          data-tooltip-variant="light"
+          onClick={handleAddToList}
+        >
+          <Check size={24} weight="bold" color="#02E7F5" />
+        </Button>
+        )}
+
+        
+
+        {!isFavorite ? (
+          <Button
           outlinedSecondary
           size="default"
           data-tooltip-id="my-tooltip"
           data-tooltip-content="Adicionar aos favoritos"
           data-tooltip-variant="light"
+          onClick={handleAddToFavorites}
         >
           <img src={star} alt="" />
         </Button>
+        ) : (
+          <Button
+          outlinedSecondary
+          size="default"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Favorito"
+          data-tooltip-variant="light"
+          onClick={handleAddToFavorites}
+        >
+          <Check size={24} weight="bold" color="#02E7F5" />
+        </Button>
+        )}
+
       </div>
     </div>
   );
