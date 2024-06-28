90ac731cfbc28fccf03d62aabf7c7cea18733655
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-06-28 17:54:42 -0300

    feat: add movie information

--- compass-video-app/src/Components/Carousel/index.tsx
+++ compass-video-app/src/Components/Carousel/index.tsx
@@ -1,48 +1,19 @@
-import { useEffect, useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { useNavigate } from 'react-router-dom';
+import { useMovieData } from '../../hooks/movies/useMovieData';
 
-interface Movie {
-  id: number;
-  title: string;
-  poster_path: string;
-}
+
 
 export function Carousel() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const VITE_API_MOVIES = import.meta.env.VITE_API_MOVIES;
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDAxYzQwNzg2YjMxNGViYjI1ZWRjY2JiZGE0NDVmNyIsIm5iZiI6MTcxOTUwODY5My4xMzU4MzMsInN1YiI6IjY2NzljNzA5YjdiYjhlY2JmZThhNGJlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.98f5UpCKgwBsLq-rCwqlVrcaFUd3Kta4-8LMcPsEGac",
-        },
-      };
-
-      try {
-        const response = await fetch(
-          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-          options
-        );
-        const data = await response.json();
-        setMovies(data.results);
-      } catch (err) {
-        console.error("Erro ao obter os filmes:", err);
-      }
-    };
-
-    fetchMovies();
-  }, [VITE_API_MOVIES]);
+  const navigate = useNavigate();
+  const { data: movies, error, isLoading } = useMovieData('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
 
   const settings = {
     dots: false,
-    infinite: false,
-    arrows: false,
+    infinite: true,
+    arrows: true,
     speed: 500,
     slidesToShow: 5.5,
     slidesToScroll: 1,
@@ -52,7 +23,7 @@ export function Carousel() {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          infinite: false,
+          infinite: true,
           dots: false,
         },
       },
@@ -73,16 +44,28 @@ export function Carousel() {
     ],
   };
 
+  const handleMovieClick = (id: number) => {
+    navigate(`/movie/${id}`);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading movies</div>;
+  }
+
   return (
     <div className="p-4">
       <h2 className="text-white text-xl font-bold">Filmes em alta</h2>
       <Slider {...settings}>
         {movies.map((movie) => (
-          <div key={movie.id} className="p-2">
+          <div key={movie.id} className="p-2" onClick={() => handleMovieClick(movie.id)}>
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
-              className="w-full h-auto gap-5 rounded-lg"
+              className="border-none cursor-pointer w-full h-auto gap-5 rounded-lg"
             />
           </div>
         ))}
