dfb0b5ceb50fc6da24433dcda1b92ad6556559de
Denilson <denilsob.b.sousa2021@gmail.com>
2024-06-30 20:40:03 -0300

    refactor: add new button styles
    add new styles in button component, use button component in login page

--- compass-video-app/src/components/Button/index.tsx
+++ compass-video-app/src/components/Button/index.tsx
@@ -4,18 +4,18 @@ import { tv, VariantProps } from 'tailwind-variants';
 
 const button = tv({
 
-    base: 'flex items-center uppercase font-worksans font-bold tracking-widest cursor-pointer box-border transition-all duration-150',
+    base: 'flex justify-center items-center uppercase font-worksans font-bold tracking-widest cursor-pointer box-border transition-all duration-150',
 
     variants: {
         size: {
             default: 'w-12 h-12 p-3 font-bold',
             sm: 'w-10 h-10 p-3',
-            md: 'px-6 py-3.5 rounded-[4px]',
+            md: 'px-6 py-3.5 rounded-[4px] my-6',
             primary: 'flex justify-center items-center p-2 md:px-4 rounded',
         },
 
         primary: {
-            true: 'bg-primary-300 text-neutral--100 text-[1rem] hover:bg-primary-200 active:bg-primary-500'
+            true: 'bg-primary-300 text-neutral--100 text-[1rem] hover:bg-primary-200 active:bg-primary-500 sm:w-[374px]'
         },
 
         secondary: {
@@ -23,11 +23,11 @@ const button = tv({
         },
 
         outlined: {
-            true: ' border-white border rounded-[4px] text-white hover:text-black!important hover:bg-black hover:text-white/90 bg-transparent hover:border-black hover:filter hover:invert hover:grayscale hover:brightness-100'
+            true: 'bg-black/10 text-neutral-100 border-white outline outline-1 outline-neutral-100 rounded-[4px] hover:text-neutral-900 hover:bg-neutral-100/40 hover:outline-none'
         },
 
         outlinedSecondary: {
-            true: ' border-white border rounded-full text-white hover:text-black!important hover:bg-black hover:text-white/90 bg-transparent hover:border-black hover:filter hover:invert hover:grayscale hover:brightness-100'
+            true: ' border-white border rounded-full text-white  hover:text-white/90 bg-black/10 hover:border-black hover:text-black!important hover:bg-black hover:filter hover:invert hover:grayscale active:bg-primary-500 '
         },
 
         outlinedCircle: {
--- compass-video-app/src/pages/Login/index.tsx
+++ compass-video-app/src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@components/Button";
 
 export function Login() {
   const tmdbAuthUrl = 'https://www.themoviedb.org/authenticate/';
@@ -50,15 +51,15 @@ export function Login() {
   };
 
   return (
-    <div className="w-full h-screen bg-cover bg-center flex justify-center items-center" style={{ backgroundImage: "url('/src/assets/Images/image-login.png')" }}>
-      <div className="w-[398px] h-[448px] bg-[rgba(53,56,67,0.79)] rounded-[20px] flex flex-col items-center p-6 sm:w-[783px]">
+    <div className="w-full h-screen bg-cover bg-center flex justify-center items-center " style={{ backgroundImage: "url('/src/assets/Images/image-login.png')" }}>
+      <div className="w-[398px] h-[448px] bg-[rgba(53,56,67,0.79)] backdrop-filter backdrop-blur-[6px] rounded-[20px] flex flex-col items-center p-6 sm:w-[783px] text-applications-high-emphasis">
         <h3 className="text-white font-bold text-[44px] mb-6 mt-8 font-work tracking-wide">Compass Video</h3>
-        <p className="text-white mb-6 text-center font-lato font-bold flex flex-col text-2xl tracking-wide">
+        <p className="text-white mb-6 text-center font-lato font-light flex flex-col text-2xl tracking-wide">
           Acesse sua conta para ver<span>nossos títulos</span>
         </p>
-        <button onClick={handleLogin} className="bg-[#037aeb] text-white px-6 rounded w-[334px] h-12 font-lato font-bold mb-6 gap-[12px] tracking-widest sm:w-[374px]">
+        <Button primary size="md" onClick={handleLogin} >
           INICIAR SESSÃO COM TMDB
-        </button>
+        </Button>
         <p className='font-lato text-[14px] text-white font-medium'>
           <span className='text-[rgba(255,255,255,0.6)] font-normal tracking-wide'>Não tem conta?</span>
           <span onClick={handleGuestLogin} className="cursor-pointer">Acesse como convidado</span>
