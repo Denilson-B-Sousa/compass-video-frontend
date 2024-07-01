a7b6a0ef1d8520cd0767024e000a031384c68821
Igor Baroni <igorbaroni77@gmail.com>
2024-07-01 16:44:00 -0300

    fix: header and footer links

--- compass-video-app/src/Components/Footer/index.tsx
+++ compass-video-app/src/Components/Footer/index.tsx
@@ -1,20 +1,23 @@
 import compassLogo from 'assets/Images/logo-compass.svg'
+import { Link } from 'react-router-dom';
 
 export function Footer() {
     return (
       <footer className="flex flex-col bg-applications-footer items-center gap-5 p-10">
+        <Link to="/">
         <img
           src={compassLogo}
           alt="Logo Compass"
           className="w-60 h-auto"
         />
+        </Link>
         <ul className="footer-text flex flex-row flex-wrap w-full justify-center gap-5">
-          <a href='#'>Política de privacidade</a>
-          <a href='#'>Contrato de assinatura</a>
-          <a href='#'>Ajuda</a>
-          <a href='#'>Dispositivos compatíveis</a>
-          <a href='#'>Sobre Disney+</a>
-          <a href='#'>Publicidade personalizada</a>
+          <Link to="/">Política de privacidade</Link>
+          <Link to="/">Contrato de assinatura</Link>
+          <Link to="/">Ajuda</Link>
+          <Link to="/">Dispositivos compatíveis</Link>
+          <Link to="/">Sobre Disney+</Link>
+          <Link to="/">Publicidade personalizada</Link>
         </ul>
         <p className="footer-text mb-4 md:px-36 lg:px-80">
           Compass Video é um serviço por inscrição paga, seu conteúdo está sujeito a disponibilidade. O serviço Compass Video é comercializado por Disney DTC LATAM, Inc., 2400 W Alameda AVE., Burbank CA 91521.
