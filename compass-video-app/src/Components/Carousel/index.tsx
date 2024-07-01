1a114e060bb1569c5b880affad53eb0036b05d63
Igor Baroni <igorbaroni77@gmail.com>
2024-07-01 16:00:47 -0300

    link fixes

--- compass-video-app/src/Components/Carousel/index.tsx
+++ compass-video-app/src/Components/Carousel/index.tsx
@@ -93,7 +93,6 @@ export function Carousel({ text, type, mediaType, mediaId, knownFor }: CarouselP
       try {
         const response = await fetch(apiUrl, options);
         const data = await response.json();
-        console.log(data)
 
         if (type === "halloweenCollection") {
           setMedia(data.items);
